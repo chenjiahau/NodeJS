1063722dab9213c7142505db0b81debaaa973c2d
chenjiahau <ivan.chen@dlinkcorp.com>
2021-12-07 19:12:33 +0800

    mini-css-extract-plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import AddHelloWorldButton from "./components/add-hello-button";
+import Heading from "./components/heading";
 
 const addHelloWorldButton = new AddHelloWorldButton();
 addHelloWorldButton.render();
+
+const heading = new Heading();
+heading.render();
