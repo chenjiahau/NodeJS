4a6e1e3fcb4d9f3ac395ff23731aee0405a9678b
chenjiahau <ivan.chen@dlinkcorp.com>
2022-03-10 09:48:48 +0800

    Rxjs

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.ts',
   entry: {
     index: "./src/index.ts"
   },
+  devtool: 'inline-source-map',
   mode: "development",
   output: {
     path: path.resolve(__dirname, "./dist"),
